63df7549728d9f44d26f0fa7f5939d0f552a13e5
Khoirony <Khoironyarief08@gmail.com>
2021-06-13 01:46:02 +0700

    menambah fitur cari data

--- headadmin.php
+++ headadmin.php
@@ -12,7 +12,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
         <form class="form-inline my-2 my-lg-0" action="surmascari.php" method="get">
-        <input class="form-control mr-sm-2" type="search" aria-label="Search">
+        <input class="form-control mr-sm-2" type="text" name="cari" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
         </form>
         <li class="nav-item ml-4">
--- surmascari.php
+++ surmascari.php
@@ -18,43 +18,28 @@
 	<title>Surat Masuk</title>
   </head>
   <body>
+
 	<div class="row no-gutters pr-3">
 		<?php
 		include('menu.php');
+		include('headadmin.php');
+		$cari = @$_GET['cari'];
 	    ?>
 		<div class="left col-md-10 p-3 mt-3">
-			<div class="mt-2 ml-5 pt-2 pl-1 mr-2"><h3><i class="far fa-paper-plane"></i> HASIL PENCARIANN DARI " "</h3> <hr></div>
+			<div class="mt-2 ml-5 pt-2 pl-1 mr-2"><h3><i class="far fa-paper-plane"></i> HASIL PENCARIANN DARI "<?php echo $cari; ?> "</h3> <hr></div>
 			<div class="ml-5 pl-1 p-3">
-				<table class="table table-borderless">
-				<tbody>
-					<tr>
-					<td style="width: 20%">Nomor Surat</td>
-					<td class="text-left"><?php echo ''.$isi['nmr_surat'].'';?></td>
-					</tr>
-					<tr>
-					<td style="width: 20%">Tanggal Surat</td>
-					<td class="text-left"><?php echo ''.$isi['tgl_surat'].'';?></td>
-					</tr>
-					<tr>
-					<td style="width: 20%">Tanggal Masuk</td>
-					<td class="text-left"><?php echo ''.$isi['tgl_masuk'].'';?></td>
-					</tr>
-					<tr>
-					<td style="width: 20%">Surat Dari</td>
-					<td class="text-left"><?php echo ''.$isi['surat_dari'].'';?></td>
-					</tr>
-					<tr>
-					<td style="width: 20%">Isi Surat</td>
-					<td class="text-left"><?php echo ''.$isi['isi'].'';?></td>
-					</tr>
-				</tbody>
-				</table>
+				<?php
+				include('sambungan.php');
+				$sql = "SELECT* FROM surat_masuk WHERE surat_dari LIKE '%".$cari."%'";
+				$query = mysqli_query($conn, $sql);
+
+				while($row = mysqli_fetch_array($query)){
+					echo 'Nomor : '.$row['nmr_surat'].' <br>';
+			
+				}?>
             </div>
 		</div>
 	</div>
-	<?php
-		include('headadmin.php');
-	?>
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
